3c7a5c9578f6308cbafc6fd9a6c6c2c90ebab227
Maria Serykh <marie.serykh@gmail.com>
2022-06-28 10:42:55 +0200

    feat: implement exit from the app, add error handling

--- index.ts
+++ index.ts
@@ -1,28 +1,58 @@
 import Jimp from 'jimp';
 import { httpServer } from './src/http_server/index';
-import robot from 'robotjs';
-import WebSocket, { createWebSocketStream, WebSocketServer } from 'ws';
+import { createWebSocketStream, WebSocketServer } from 'ws';
 
 import { runCommand } from './src/modules/runCommand';
 
 const HTTP_PORT = 3000;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
+console.log(`Start static http server on the ${HTTP_PORT} port`);
 httpServer.listen(HTTP_PORT);
 
 const WS_PORT = 8080;
 
 const webSocketServer = new WebSocketServer({ port: WS_PORT });
 
-console.log(`Start ws server on the ${WS_PORT} port!`);
+console.log(`Start web socket server on the ${WS_PORT} port`);
+
 webSocketServer.on('connection', (ws) => {
-  ws.on('message', (data) => {
-    const input = data.toString();
-    runCommand(input);
-    console.log('received: %s', input);
+  console.log('A client has just connected');
+
+  const duplex = createWebSocketStream(ws, { encoding: 'utf8', decodeStrings: false });
+
+  duplex.on('data', async (data) => {
+    try {
+      const input = data.toString();
+      const output = await runCommand(input);
+      console.log('< -', input);
+      
+      const result = `${input} ${output ? output : ''}\0`;
+
+      if (typeof result === 'string') {
+        console.log('- >', result);
+
+        duplex.write(result);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  ws.on('error', (e) => {
+    console.log(e);
+  });
+
+  ws.on('close', () => {
+    console.log('Websocket has been closed');
   });
 });
 
 webSocketServer.on('close', () => {
-  console.log('ws connection is closed');
+  console.log('Web socket connection is closed');
+});
+
+process.on('SIGINT', () => {
+  console.log('Web socket server will be closed');
+  webSocketServer.close();
+  process.exit();
 });
