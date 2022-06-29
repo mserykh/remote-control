5c03abccab892c4c1856a7995814ba1f61dbf2e7
Maria Serykh <marie.serykh@gmail.com>
2022-06-29 09:20:44 +0200

    refactor: refactor drawing a circle

--- src/modules/drawCircle.ts
+++ src/modules/drawCircle.ts
@@ -1,21 +1,20 @@
 import robot from 'robotjs';
 import { moveMouse } from './moveMouse';
 import { moveRight } from './moveRight';
+import { moveUp } from './moveUp';
 
 export const drawCircle = async (args: Array<string>) => {
   const radiusRaw = args[0];
   const radius = Number(radiusRaw);
 
-  // const currPos = robot.getMousePos();
-  // moveMouse(-currPos.x, 0);
   const center = robot.getMousePos();
 
-  robot.mouseToggle('down');
-
-  for (let i = -90; i < 270; i++) {
+  for (let i = -90; i <= 270; i++) {
     const x = radius * Math.cos((i * Math.PI) / 180) + center.x;
     const y = radius * Math.sin((i * Math.PI) / 180) + center.y;
+
     robot.dragMouse(x, y);
+    if (i === -90) robot.mouseToggle('down');
   }
 
   robot.mouseToggle('up');
